cdf771786d6048178ed44b6fb72ca23a68b7e604
Earl DG <161915974+AyeItsPearl@users.noreply.github.com>
2024-10-21 22:02:54 +0800

    Add files via upload

---
+++ Arithmetic11.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	cout <<"Enter your First Number" <<endl;
+	cout <<"First Number : ";
+	float a;
+	cin>>a;
+	cout <<"Enter your Second Number" <<endl;
+	cout <<"Second Number : ";
+	float b;
+	cin>>b;
+	
+	cout <<"The Sum of " <<a <<" and " <<b <<" is equal to " <<a+b <<endl;
+	cout <<"The Difference of " <<a <<" and " <<b <<" is equal to " <<a-b <<endl;
+	cout <<"The Product of " <<a <<" and " <<b <<" is equal to " <<a*b <<endl;
+	cout <<"The Quotient and Remainder of " <<a <<" and " <<b <<" is equal to " <<a/b <<endl;
+	
+	return 0;  
+}
+
